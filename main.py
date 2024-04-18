e0e46da4389b80b078c1fb76749c67f1bbd74265
User <dgfdsg@gmail.com>
2024-04-18 11:27:01 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+
+# Вместо BOT TOKEN HERE нужно вставить токен вашего бота, полученный у @BotFather
+BOT_TOKEN = '7157534842:AAGuExWpdrTvidFSCi17FIFxOln5gTwaHns'
+
+# Создаем объекты бота и диспетчера
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+# Этот хэндлер будет срабатывать на команду "/start"
+@dp.message(Command(commands=["start"]))
+async def process_start_command(message: Message):
+    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
+
+
+# Этот хэндлер будет срабатывать на команду "/help"
+@dp.message(Command(commands=['help']))
+async def process_help_command(message: Message):
+    await message.answer(
+        'Напиши мне что-нибудь и в ответ '
+        'я пришлю тебе твое сообщение'
+    )
+
+
+# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
+# кроме команд "/start" и "/help"
+@dp.message()
+async def send_echo(message: Message):
+    chat_id = message.chat.id
+    first_name = message.chat.first_name
+    await message.reply(f"Привет, {first_name}! {chat_id}")
+
+
+
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
